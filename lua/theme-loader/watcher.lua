4d7600f893a1fa3bcb0b40db1f44637dc32fdaa0
Suchith Sridhar <suchith.sridhar@gmail.com>
2025-01-04 18:25:59 -0400

    fixing watch functionality

--- lua/theme-loader/watcher.lua
+++ lua/theme-loader/watcher.lua
@@ -1,21 +1,46 @@
 local M = {}
+local debounce_timer = nil
+
+-- Debounce function to prevent frequent reloads
+local function debounce(callback, delay)
+	if debounce_timer then
+		debounce_timer:stop()
+	end
+
+	debounce_timer = vim.loop.new_timer()
+	debounce_timer:start(delay, 0, function()
+		vim.schedule(function()
+			callback() -- Safely execute the callback on the main thread
+		end)
+		debounce_timer:stop()
+		debounce_timer:close()
+		debounce_timer = nil
+	end)
+end
 
 function M.watch(theme_file, callback)
 	local has_fwatch, fwatch = pcall(require, "fwatch")
 	if not has_fwatch then
-		vim.notify("Theme-loader: 'fwatch.nvim' is not installed. File watching disabled.", vim.log.levels.WARN)
+		vim.schedule(function()
+			vim.notify("Theme-loader: 'fwatch.nvim' is not installed. File watching disabled.", vim.log.levels.WARN)
+		end)
 		return
 	end
 
 	-- Watch for changes using fwatch.nvim
 	fwatch.watch(theme_file, {
 		on_event = function()
-			callback()
-			vim.notify("Theme-loader: Reloaded theme due to changes in " .. theme_file)
+			-- Debounce and schedule the callback function
+			debounce(function()
+				vim.schedule(function()
+					callback() -- Reload theme safely
+					vim.schedule(function()
+						vim.api.nvim_echo({ { "Theme-loader: theme reloaded.", "None" } }, false, {})
+					end)
+				end)
+			end, 1000) -- 1000ms debounce time
 		end,
 	})
-
-	vim.notify("Theme-loader: Watching for changes in " .. theme_file .. " using fwatch.nvim")
 end
 
 return M
