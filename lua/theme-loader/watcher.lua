fef312468d3517d7dac052013b9b7ebc7cfdc8e9
Suchith Sridhar <suchith.sridhar@gmail.com>
2025-01-04 18:11:33 -0400

    updating file watching

--- lua/theme-loader/watcher.lua
+++ lua/theme-loader/watcher.lua
@@ -1,14 +1,21 @@
 local M = {}
 
 function M.watch(theme_file, callback)
-	vim.api.nvim_create_autocmd("BufWritePost", {
-		pattern = theme_file,
-		callback = function()
+	local has_fwatch, fwatch = pcall(require, "fwatch")
+	if not has_fwatch then
+		vim.notify("Theme-loader: 'fwatch.nvim' is not installed. File watching disabled.", vim.log.levels.WARN)
+		return
+	end
+
+	-- Watch for changes using fwatch.nvim
+	fwatch.watch(theme_file, {
+		on_event = function()
 			callback()
+			vim.notify("Theme-loader: Reloaded theme due to changes in " .. theme_file)
 		end,
 	})
 
-	vim.notify("Theme-loader: Watching for changes in " .. theme_file)
+	vim.notify("Theme-loader: Watching for changes in " .. theme_file .. " using fwatch.nvim")
 end
 
 return M
